b42da354399259921338319749776ec51f771489
joangao <64297106+Joan-gao@users.noreply.github.com>
2024-02-15 10:22:46 +0800

    final commit

---
+++ src/components/Modal.js
@@ -0,0 +1,19 @@
+import './Modal.css'
+import  ReactDOM  from 'react-dom'
+
+export default function Modal({ children, isSalesModal }) {
+  return ReactDOM.createPortal((
+    <div className="modal-backdrop">
+      <div className="modal" style={{
+        border: "4px solid", 
+        borderColor: isSalesModal ? "#ff4500" : "#555",
+        textAlign: "center"
+      }}>
+        {children}
+        <button
+        className={isSalesModal ? "sales-btn" : ""}
+        >close</button>
+      </div>
+    </div>
+  ), document.body)
+}
